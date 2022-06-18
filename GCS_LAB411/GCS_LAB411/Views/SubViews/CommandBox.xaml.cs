da868df039727118375142ab5b849522d781d96c
PhanTuanHust2018 <tuan23012000@gmail.com>
2022-06-18 15:53:16 +0700

    control bar design

---
+++ GCS_LAB411/GCS_LAB411/Views/SubViews/CommandBox.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GCS_LAB411.Views.SubViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CommandBox : ContentView
+    {
+        public CommandBox()
+        {
+            InitializeComponent();
+            //this.BindingContext = new CommandViewModel();
+        }
+    }
+}
